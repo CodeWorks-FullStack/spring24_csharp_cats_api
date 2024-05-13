37f051f890ff3632c1590e98bffe13932b113f42
Jeremy Fowler <jembyfowler@gmail.com>
2024-05-13 11:13:48 -0600

    Created cat model

--- Controllers/CatsController.cs
+++ Controllers/CatsController.cs
@@ -16,6 +16,4 @@ public class CatsController : ControllerBase
   }
 
 
-
-
 }
---
+++ Models/Cat.cs
@@ -0,0 +1,10 @@
+namespace csharp_cats_api.Models;
+
+public class Cat
+{
+  public int Id { get; set; }
+  public string Name { get; set; }
+  public int Age { get; set; }
+  public bool HasPolydactylity { get; set; }
+  public string Color { get; set; }
+}
